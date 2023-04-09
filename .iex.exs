d57af5c10ad436e101d64f3ba03b83e7a1558422
Adriano Mota <adrianowsh@hotmail.com>
2023-04-08 21:23:49 -0300

    start

---
+++ .iex.exs
@@ -0,0 +1,11 @@
+import Ecto.Query
+alias RentCars.Repo
+
+# import RentCar.UsersFixtures
+# import RentCar.AccountsFixtures
+
+# alias RentCar.Accounts
+# alias RentCar.Users
+# alias RentCar.Admins
+# alias RentCar.Emails
+# alias RentCar.Mailer
